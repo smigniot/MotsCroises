791f74ebd4fbe0f9d17d0f58bce4bc79c24a00b6
Sebastien <sebastien@migniot.com>
2024-09-22 08:11:14 +0200

    Dawg generated

--- generate.js
+++ generate.js
@@ -1,45 +1,14 @@
 const fs = require('fs');
+const dawg = require('dawg-lookup');
 
 const words = fs.readFileSync("dictionary.txt",'utf8')
     .trim().split(/[\r\n]/gm)
     .map(l=>l.trim()).filter(l=>l)
     ;
-const payload = fs.readFileSync(process.argv[2],'utf8')
-    ;
-const {w:width,h:height,m:grid} = payload
-    .trim().split(/[\r\n]/gm)
-    .filter(l=>l.trim())
-    .reduce((acc,line)=>{
-        acc.h++;
-        acc.w = Math.max(acc.w,line.length);
-        acc.m.push(line.match(/./gm));
-        return acc;
-    },{h:0,w:0,m:[]});
 
-console.log("WORDS", words.slice(0,8).join(" "), "...");
-console.log("WIDTHxHEIGHT", width, height);
-console.log("GRID", [""].concat(grid.map(r=>r.join(""))).join("\n "));
+const trie = new dawg.Trie(words.join(" "));
+const packed = trie.pack();
 
-console.time("GADDAG");
-const gaddag = words.reduce((root,word)=>{
-    const n = word.length;
-    for(let i=1; i<n; i++) {
-        const rev = (word.slice(0,i).match(/./gm) || []).reverse().join("");
-        const suffix = word.slice(i);
-        const revxy = rev+(suffix?"+":"")+suffix+"#";
-        revxy.match(/./gm).reduce((current,letter)=>{
-            if(! current[letter]) {
-                current[letter] = {};
-            }
-            return current[letter];
-        },root);
-    }
-    return root;
-},{});
-console.timeEnd("GADDAG");
-
-console.time("WRITE");
-fs.writeFileSync("gaddag.json", JSON.stringify(gaddag));
-console.timeEnd("WRITE");
+fs.writeFileSync("dawg.fr.txt", JSON.stringify(packed));
 
 
