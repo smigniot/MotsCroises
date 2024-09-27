26223b9b67a8b0e8ea608eafbb4c90e690c03179
Sebastien <sebastien@migniot.com>
2024-09-27 16:55:34 +0200

    Web ui/ux wip

--- loader.js
+++ loader.js
@@ -41,25 +41,92 @@ function dictionaryReceived(words) {
  * At this point, the editor is under building
  * After building, messages can flow to the worker
  */
+const seq = (n)=>[...Array(n).keys()];
 function enableEditor() {
+    const select = document.querySelector("#grid_layout");
+    select.disabled = false;
+    select.addEventListener("change",function() {
+        switch(select.value) {
+            case "5x5":
+            case "6x6":
+            case "10x10":
+            case "15x15":
+                const [w,h] = select.value.split(/x/gm).map(s=>+s);
+                applyLayout(seq(h).map(_=>
+                        seq(w).map(_=>".").join("")
+                    ).join("\n"));
+                break;
+            case "Dog":
+                applyLayout([
+                    "  ## ##### ##  ",
+                    " #..#.....#..# ",
+                    "#.............#",
+                    "#..#.......#..#",
+                    "#.##.#...#.##.#",
+                    "## #.#...#.# ##",
+                    "   #.......#   ",
+                    "   #..###..#   ",
+                    "    #..#..#    ",
+                    "     #####     ",
+                ].join("\n"));
+                break;
+            case "Cat":
+                applyLayout([
+                    "##        ##",
+                    "#.#      # #",
+                    "#..#....#..#",
+                    "#...####...#",
+                    "#..........#",
+                    "#..#....#..#",
+                    "#.#.#..#.#.#",
+                    "#..........#",
+                    " #...##...# ",
+                    "  #......#  ",
+                    "   ######   ",
+                ].join("\n"));
+                break;
+            case "American":
+                applyLayout([
+                    "....#....#.....",
+                    "....#....#.....",
+                    "....#....#.....",
+                    "...........#...",
+                    "###...#........",
+                    "........#....##",
+                    ".........##....",
+                    "...#...#...#...",
+                    "....##.........",
+                    "##....#........",
+                    "........#...###",
+                    "...#...........",
+                    ".....#....#....",
+                    ".....#....#....",
+                    ".....#....#....",
+                ].join("\n"));
+                break;
+        }
+    });
+}
+function applyLayout(layout) {
     const root = document.querySelector(".grid");
-    const wi = document.querySelector("#grid_width");
-    const hi = document.querySelector("#grid_height");
-    const li = document.querySelector("#grid_layout");
-    li.onchange = function() {
-        console.log("LAYOUT", li.value);
+    while(root.firstChild){
+        root.removeChild(root.firstChild);
     }
-    wi.onchange = function() {
-        whChanged();
-    }
-    hi.onchange = function() {
-        whChanged();
-    }
-    function whChanged() {
-        const w = (+wi.value) || 6;
-        const h = (+hi.value) || 6;
-        console.log("SIZE", `${w}x${h}`);
-    }
-
+    const table = root.appendChild(document.createElement("table"));
+    table.className = "grid";
+    table.border = "1";
+    layout.split(/[\r\n]+/gm).forEach(row=>{
+        const tr = table.appendChild(document.createElement("tr"));
+        row.match(/./gm).forEach(cell=>{
+            const td = tr.appendChild(document.createElement("td"));
+            td.textContent = cell;
+            if(cell == '.') {
+                td.textContent = "";
+            } else if(cell == '#') {
+                td.className = "black";
+            }
+        });
+    });
+    console.log("Applying layout", layout);
 }
 
